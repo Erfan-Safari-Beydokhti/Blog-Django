d348b813995a09f3177119e0231f62817a66ab39
Erfan <erfansafari.b.86@gmail.com>
2025-06-09 18:26:14 +0300

    write some data for each course in views for Use dynamic

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
-from django.views import View
+from datetime import date
+all_posts=[
+    {
+        'slug':'learning-django',
+        'title':'django course',
+        'author':'Erfan Safari',
+        'image':'img.png',
+        'date':date(2025,6,9),
+        'short_description':'This is Django Course for Beginner',
+        'content':"""
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque enim itaque repellat! A aperiam
+        """
+    },
+{
+        'slug':'learning-python',
+        'title':'python course',
+        'author':'Erfan Safari',
+        'image':'img_1.png',
+        'date':date(2025,6,11),
+        'short_description':'This is Python Course for Beginner',
+        'content':"""
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque enim itaque repellat! A aperiam
+        """
+    },
+{
+        'slug':'learning-MySQL',
+        'title':'MySQL course',
+        'author':'Erfan Safari',
+        'image':'img_2.png',
+        'date':date(2025,6,22),
+        'short_description':'This is MySQL Course for Beginner',
+        'content':"""
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque enim itaque repellat! A aperiam
+        """
+    },
 
+]
 def IndexView(request):
     return render(request, 'blog/index.html')
 def PostsView(request):
